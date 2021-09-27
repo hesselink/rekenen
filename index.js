323054ed130f522059d74b7d3df82dc518680cb6
Erik Hesselink <hesselink@gmail.com>
2021-09-27 20:43:36 +0200

    Keer

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+let configurations =
+  [ { lower: 1
+    , upper: 999
+    , operator: '+'
+    }
+  , { lower: 1
+    , upper: 10
+    , operator: '*'
+    }
+  ]
 let answer;
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -16,11 +26,32 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function refresh() {
+  const configuration = pick(configurations);
   const number1 = document.getElementById("number1");
   const number2 = document.getElementById("number2");
-  const x = Math.floor(Math.random() * 1000);
-  const y = Math.floor(Math.random() * 1000);
+  const operator = document.getElementById("operator");
+  const x = random(configuration.lower, configuration.upper);
+  const y = random(configuration.lower, configuration.upper);
   number1.textContent = x;
   number2.textContent = y;
-  answer = x + y
+  operator.textContent = configuration.operator;
+  answer = calculate(configuration.operator, x, y);
+}
+
+function pick(xs) {
+  const ix = random(0, xs.length - 1);
+  return xs[ix];
+}
+
+// random integer from 'from' to 'to' (inclusive, [from, to])
+function random(from, to) {
+  return Math.floor(Math.random() * (to - from + 1)) + from;
+}
+
+function calculate(operator, x, y) {
+  switch(operator) {
+    case '+': return x + y;
+    case '*': return x * y;
+    case '-': return x - y;
+  }
 }
